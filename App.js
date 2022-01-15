9cba54f952e840682bee85975687af7d2704ae2f
Dipanshu <dipanshukubde5678@gmail.com>
2022-01-15 17:45:18 +0530

    basic

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import { store } from './store';
+import HomeScreen from './screens/HomeScreen';
 
 export default function App() {
   return (
-    <Provider>
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <Provider store={store}>
+      <HomeScreen />
+    {/* <View style={styles.container}>
+      <Text>Lets Build Uber</Text>
       <StatusBar style="auto" />
-    </View>
+    </View> */}
     </Provider>
   );
 }
